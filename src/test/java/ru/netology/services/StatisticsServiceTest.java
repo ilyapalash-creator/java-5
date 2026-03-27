1e3cc5d60be04a89d560efe3424b849c0ef61de7
Ilya Palash <rudut.ilya@ya.ru>
2026-03-27 16:00:43 +0300

    CSV test

--- src/test/java/ru/netology/services/StatisticsServiceTest.java
+++ src/test/java/ru/netology/services/StatisticsServiceTest.java
@@ -2,24 +2,27 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class StatisticsServiceTest {
 
-    @Test
-    public void testCalculateWithFirtExample(){
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/statistics.csv")
+    public void testCalculateWithFirstExample(int income, int expenses, int threshold, int expected) {
         StatisticsService service = new StatisticsService();
-        int actual = service.calculate(10_000,3_000,20_000);
-        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testCalculateWithSecondExample(){
-        StatisticsService service = new StatisticsService();
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
-    }
-
-
+    //  @Test
+    //  public void testCalculateWithSecondExample(){
+    //      StatisticsService service = new StatisticsService();
+    //      int actual = service.calculate(100_000, 60_000, 150_000);
+    //      int expected = 2;
 }
+
+
+
